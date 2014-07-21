f5fdf5148588980dd051f77c644467eb4c577a23
rainabba <rainabba@gmail.com>
2014-07-21 14:39:41 -0700

    Update main.js
    Added callback test.

--- hello-world/main.js
+++ hello-world/main.js
@@ -19,5 +19,8 @@ chrome.app.runtime.onLaunched.addListener(function() {
       left: Math.round((screenWidth-width)/2),
       top: Math.round((screenHeight-height)/2)
     }
+  },
+  function(win) {
+    console.log("BOOYAH", win);
   });
 });
